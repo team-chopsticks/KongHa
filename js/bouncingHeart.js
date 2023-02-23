3565f31f5b3a24230c5ff6cdf80ac749ef406ffb
nareeshin <hallo.nareeshin@gmail.com>
2023-02-23 20:40:49 +0900

    하트 7마리, 토글 스위치 기능 추가

--- js/bouncingHeart.js
+++ js/bouncingHeart.js
@@ -10,9 +10,25 @@ main.style.width = "innerWidth";
 main.style.height = "innerHeight";
 
 const pinkHeart01 = document.createElement("div");
-const b = {x:0, y:0, w:150, h:143, dx:1, dy:1, speed:3.5, ani:{}, move:false};
+const b = {x:0, y:0, w:150, h:143, dx:1, dy:1, speed:3.7, ani:{}, move:false};
 
-const g = {x:300, y:500, w:150, h:143, dx:1, dy:1, speed:3.5, ani:{}, move:false};
+const pinkHeart02 = document.createElement("div");
+const g = {x:1000, y:500, w:150, h:143, dx:1, dy:1, speed:2.5, ani:{}, move:false};
+
+const pinkHeart03 = document.createElement("div");
+const c = {x:600, y:300, w:150, h:143, dx:1, dy:-1, speed:3, ani:{}, move:false};
+
+const pinkHeart04 = document.createElement("div");
+const a = {x:900, y:600, w:150, h:143, dx:-1, dy:-1, speed:3, ani:{}, move:false};
+
+const greenHeart01 = document.createElement("div");
+const d = {x:200, y:200, w:150, h:143, dx:1, dy:-1, speed:2, ani:{}, move:false};
+
+const greenHeart02 = document.createElement("div");
+const e = {x:800, y:100, w:150, h:143, dx:1, dy:-1, speed:3.5, ani:{}, move:false};
+
+const greenHeart03 = document.createElement("div");
+const f = {x:1000, y:400, w:150, h:143, dx:1, dy:-1, speed:2, ani:{}, move:false};
 
 pinkHeart01.className = "pinkHeart";
 pinkHeart01.style.width = `${b.w}px`;
@@ -21,16 +37,56 @@ pinkHeart01.style.position ="absolute";
 pinkHeart01.style.left = `${b.x}px`;
 pinkHeart01.style.top = `${b.y}px`;
 
-const greenHeart01 = document.createElement("div");
+pinkHeart02.className = "pinkHeart";
+pinkHeart02.style.width = `${g.w}px`;
+pinkHeart02.style.height = `${g.h}px`;
+pinkHeart02.style.position ="absolute";
+pinkHeart02.style.left = `${g.x}px`;
+pinkHeart02.style.top = `${g.y}px`;
+
+pinkHeart03.className = "pinkHeart";
+pinkHeart03.style.width = `${c.w}px`;
+pinkHeart03.style.height = `${c.h}px`;
+pinkHeart03.style.position ="absolute";
+pinkHeart03.style.left = `${c.x}px`;
+pinkHeart03.style.top = `${c.y}px`;
+
+pinkHeart04.className = "pinkHeart";
+pinkHeart04.style.width = `${a.w}px`;
+pinkHeart04.style.height = `${a.h}px`;
+pinkHeart04.style.position ="absolute";
+pinkHeart04.style.left = `${a.x}px`;
+pinkHeart04.style.top = `${a.y}px`;
+
 greenHeart01.className = "greenHeart";
-greenHeart01.style.width = `${g.w}px`;
-greenHeart01.style.height = `${g.h}px`;
+greenHeart01.style.width = `${d.w}px`;
+greenHeart01.style.height = `${d.h}px`;
 greenHeart01.style.position ="absolute";
-greenHeart01.style.left = `${g.x}px`;
-greenHeart01.style.top = `${g.y}px`;
+greenHeart01.style.left = `${d.x}px`;
+greenHeart01.style.top = `${d.y}px`;
+
+greenHeart02.className = "greenHeart";
+greenHeart02.style.width = `${e.w}px`;
+greenHeart02.style.height = `${e.h}px`;
+greenHeart02.style.position ="absolute";
+greenHeart02.style.left = `${e.x}px`;
+greenHeart02.style.top = `${e.y}px`;
+
+greenHeart03.className = "greenHeart";
+greenHeart03.style.width = `${f.w}px`;
+greenHeart03.style.height = `${f.h}px`;
+greenHeart03.style.position ="absolute";
+greenHeart03.style.left = `${f.x}px`;
+greenHeart03.style.top = `${f.y}px`;
+
 
 main.append(pinkHeart01);
+main.append(pinkHeart02);
+main.append(pinkHeart03);
+main.append(pinkHeart04);
 main.append(greenHeart01);
+main.append(greenHeart02);
+main.append(greenHeart03);
 
 mover();
 
@@ -47,15 +103,70 @@ function mover(){
     if(g.y>innerHeight-g.h ||g.y < 0){
         g.dy *= -1;
     }
+    if(c.x>innerWidth-c.w || c.x < 0){
+        c.dx *= -1;
+    }
+    if(c.y>innerHeight-c.h ||c.y < 0){
+        c.dy *= -1;
+    }
+    if(a.x>innerWidth-a.w || a.x < 0){
+        a.dx *= -1;
+    }
+    if(a.y>innerHeight-a.h ||a.y < 0){
+        a.dy *= -1;
+    }
+
+    if(d.x>innerWidth-d.w || d.x < 0){
+        d.dx *= -1;
+    }
+    if(d.y>innerHeight-d.h ||d.y < 0){
+        d.dy *= -1;
+    }
+    if(e.x>innerWidth-e.w || e.x < 0){
+        e.dx *= -1;
+    }
+    if(e.y>innerHeight-e.h ||e.y < 0){
+        e.dy *= -1;
+    }
+    if(f.x>innerWidth-f.w || f.x < 0){
+        f.dx *= -1;
+    }
+    if(f.y>innerHeight-f.h ||f.y < 0){
+        f.dy *= -1;
+    }
+
     b.x += b.dx * b.speed;
     b.y += b.dy * b.speed;
     g.x += g.dx * g.speed;
     g.y += g.dy * g.speed;
+    c.x += c.dx * c.speed;
+    c.y += c.dy * c.speed;
+    a.x += a.dx * a.speed;
+    a.y += a.dy * a.speed;
+
+    d.x += d.dx * d.speed;
+    d.y += d.dy * d.speed;
+    e.x += e.dx * e.speed;
+    e.y += e.dy * e.speed;
+    f.x += f.dx * f.speed;
+    f.y += f.dy * f.speed;
+
 
     pinkHeart01.style.left = `${b.x}px`;
     pinkHeart01.style.top = `${b.y}px`;
-    greenHeart01.style.left = `${g.x}px`;
-    greenHeart01.style.top = `${g.y}px`;
+    pinkHeart02.style.left = `${g.x}px`;
+    pinkHeart02.style.top = `${g.y}px`;
+    pinkHeart03.style.left = `${c.x}px`;
+    pinkHeart03.style.top = `${c.y}px`;
+    pinkHeart04.style.left = `${a.x}px`;
+    pinkHeart04.style.top = `${a.y}px`;
+
+    greenHeart01.style.left = `${d.x}px`;
+    greenHeart01.style.top = `${d.y}px`;
+    greenHeart02.style.left = `${e.x}px`;
+    greenHeart02.style.top = `${e.y}px`;
+    greenHeart03.style.left = `${f.x}px`;
+    greenHeart03.style.top = `${f.y}px`;
 
         b.ani = requestAnimationFrame(mover);
 }
--- js/script.js
+++ js/script.js
@@ -8,3 +8,56 @@
 // });
 
 
+
+const switch_color = document.getElementById("switch_color");
+const div2 = document.querySelector(".div2");
+const div3 = document.querySelector(".div3");
+const div4 = document.querySelector(".div4");
+const div5 = document.querySelector(".div5");
+const div6 = document.querySelector(".div6");
+const div7 = document.querySelector(".div7");
+
+
+switch_color.addEventListener("click", function() {
+  if (switch_color.checked) {
+    div3.style.backgroundColor = "#43FF8F";
+    div6.style.backgroundColor = "#43FF8F";
+    div4.style.backgroundColor = "#FF33D9";
+    div5.style.backgroundColor = "#FF33D9";
+    div2.style.background = "linear-gradient(270deg, rgba(255, 51, 217, 0.5) 0%, rgba(255, 255, 255, 1) 100%)";
+    div7.style.background = "linear-gradient(90deg, rgba(67, 255, 143, 0.8) 0%, rgba(255, 255, 255, 1) 100%)";
+} else {
+    div3.style.backgroundColor = "";
+    div4.style.backgroundColor = "";
+    div5.style.backgroundColor = "";
+    div6.style.backgroundColor = "";
+    div2.style.background = "";
+    div7.style.background = "";
+  }
+});
+
+const switch_language = document.getElementById("switch_language");
+const div2_text = document.querySelector("#div2");
+const div3_text = document.querySelector("#div3");
+const div4_text = document.querySelector("#div4");
+const div5_text = document.querySelector("#div5");
+const div6_text = document.querySelector("#div6");
+const div7_text = document.querySelector("#div7");
+
+switch_language.addEventListener("click", function() {
+    if (switch_language.checked) {
+        div2_text.innerText = "COMMISSIONED\nPROJECTS";
+        div3_text.innerText = "ABOUT";
+        div4_text.innerText = "CONTACT";
+        div5_text.innerText = "INSTAGRAM";
+        div6_text.innerText = "INSPIRATIONS";
+        div7_text.innerText = "KONG & HA\nPROJECTS";
+    } else {
+        div2_text.innerText = "콩과하의\n자체 프로젝트";
+        div3_text.innerText = "콩과하에 대하여";
+        div4_text.innerText = "프로젝트 문의";
+        div5_text.innerText = "인스타그램";
+        div6_text.innerText = "영감의 원천";
+        div7_text.innerText = "클라이언트\n프로젝트";
+    }
+  });
