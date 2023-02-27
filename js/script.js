a7c19403f6d28e5fa5910be7d42fd4a0638bb630
heydahyun <hwangdahyun0715@gmail.com>
2023-02-27 21:21:03 +0100

    Sub Pages
    레이아웃, 링크 연결 등

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-
-
-// $('.marquee').marquee({
-//     duration: 10000,
-//     pauseOnHover: true,
-//     gap: 50,
-//     duplicated: true
-// });
-
-// const marqueeContainer = document.querySelector('.sub_1');
-// const marqueeItem = document.querySelector('.marquee-item');
-
-// Set the width of the marquee item to match the width of the container
-// marqueeItem.style.width = `${sub_1.offsetWidth}px`;
-
-
-
 const switch_color = document.getElementById("switch_color");
 const div2 = document.querySelector(".div2");
 const div3 = document.querySelector(".div3");
@@ -24,7 +7,7 @@ const div6 = document.querySelector(".div6");
 const div7 = document.querySelector(".div7");
 
 
-switch_color.addEventListener("click", function() {
+switch_color.addEventListener("click", function () {
   if (switch_color.checked) {
     div3.style.backgroundColor = "#43FF8F";
     div6.style.backgroundColor = "#43FF8F";
@@ -32,7 +15,7 @@ switch_color.addEventListener("click", function() {
     div5.style.backgroundColor = "#FF33D9";
     div2.style.background = "linear-gradient(270deg, rgba(255, 51, 217, 0.5) 0%, rgba(255, 255, 255, 1) 100%)";
     div7.style.background = "linear-gradient(90deg, rgba(67, 255, 143, 0.8) 0%, rgba(255, 255, 255, 1) 100%)";
-} else {
+  } else {
     div3.style.backgroundColor = "";
     div4.style.backgroundColor = "";
     div5.style.backgroundColor = "";
@@ -42,7 +25,11 @@ switch_color.addEventListener("click", function() {
   }
 });
 
+// Language Switch -> html 연동 되도록. 커비와도!
+
 const switch_language = document.getElementById("switch_language");
+const sub1_text = document.querySelector("#sub1");
+const sub2_text = document.querySelector("#sub2");
 const div2_text = document.querySelector("#div2");
 const div3_text = document.querySelector("#div3");
 const div4_text = document.querySelector("#div4");
@@ -50,39 +37,62 @@ const div5_text = document.querySelector("#div5");
 const div6_text = document.querySelector("#div6");
 const div7_text = document.querySelector("#div7");
 
-switch_language.addEventListener("click", function() {
-    if (switch_language.checked) {
-        div2_text.innerText = "COMMISSIONED\nPROJECTS";
-        div3_text.innerText = "ABOUT";
-        div4_text.innerText = "CONTACT";
-        div5_text.innerText = "INSTAGRAM";
-        div6_text.innerText = "INSPIRATIONS";
-        div7_text.innerText = "KONG & HA\nPROJECTS";
-    } else {
-        div2_text.innerText = "콩과하의\n자체 프로젝트";
-        div3_text.innerText = "콩과하에 대하여";
-        div4_text.innerText = "프로젝트 문의";
-        div5_text.innerText = "인스타그램";
-        div6_text.innerText = "영감의 원천";
-        div7_text.innerText = "클라이언트\n프로젝트";
-    }
-  });
-
-
-
-  function moveDiv() {
-    var wrap = document.querySelector('.wrap');
-    var div8 = document.querySelector('#myDiv');
-    var mobile = div8.querySelector('.mobile');
-    var screenWidth = window.innerWidth;
-
-    if (screenWidth < 820) {
-      mobile.appendChild(wrap);
-    } else {
-      var sub3 = document.querySelector('.sub_3');
-      sub3.insertBefore(wrap, sub3.firstChild);
-    }
+switch_language.addEventListener("click", function () {
+  if (switch_language.checked) {
+    sub1_text.innerText = "Hit the hearts!";
+    sub2_text.innerText = "KONG & HA do this and that in various ways.";
+    div2_text.innerText = "COMMISSIONED\nPROJECTS";
+    div3_text.innerText = "ABOUT";
+    div4_text.innerText = "CONTACT";
+    div5_text.innerText = "INSTAGRAM";
+    div6_text.innerText = "INSPIRATIONS";
+    div7_text.innerText = "KONG & HA\nPROJECTS";
+  } else {
+    sub1_text.innerText = "하트를 때려보세요!";
+    sub2_text.innerText = "콩과하는 이런 일 저런 일 이렇게 저렇게 합니다.";
+    div2_text.innerText = "콩과하의\n자체 프로젝트";
+    div3_text.innerText = "콩과하에 대하여";
+    div4_text.innerText = "프로젝트 문의";
+    div5_text.innerText = "인스타그램";
+    div6_text.innerText = "영감의 원천";
+    div7_text.innerText = "클라이언트\n프로젝트";
   }
+});
 
-  window.addEventListener('load', moveDiv);
-  window.addEventListener('resize', moveDiv);
+// 스위치 토글시 영문 국문 바뀌게
+
+const switchLanguage = document.getElementById('switch_language');
+const languageText = document.getElementById('language_text');
+
+switchLanguage.addEventListener('change', function () {
+    if (this.checked) {
+        languageText.textContent = 'EN';
+    } else {
+        languageText.textContent = 'KR';
+    }
+});
+
+// 스크린 820 보다 작아지면 mobile 클래스로 이동하여 레이아웃 바뀌게
+
+function moveDiv() {
+  var wrap = document.querySelector('.wrap');
+  var div8 = document.querySelector('#myDiv');
+  var mobile = div8.querySelector('.mobile');
+  var screenWidth = window.innerWidth;
+
+  if (screenWidth < 820) {
+    mobile.appendChild(wrap);
+  } else {
+    var sub3 = document.querySelector('.sub_3');
+    sub3.insertBefore(wrap, sub3.firstChild);
+  }
+}
+
+window.addEventListener('load', moveDiv);
+window.addEventListener('resize', moveDiv);
+
+// 윈도우 리사이즈 -> 리로드 되는 것인데... 영문/국문 스위치 되었을 때 다 리로드가 되면 안될 것 같다. 그럼 다시 국문으로 돌아오네...? html을 바꾸면 괜찮으려나!?
+
+window.addEventListener("resize", function () {
+  location.reload();
+});
